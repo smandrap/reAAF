56ba758dc6741fa56068d20599bbc8907090166b
Federico Manuppella <mnp.federico@gmail.com>
2026-03-23 23:05:41 +0100

    feat: zoom to project on completed import

--- src/main.cpp
+++ src/main.cpp
@@ -31,10 +31,39 @@
 
 REAPER_PLUGIN_HINSTANCE g_hInst = nullptr;
 
+// This stuff is needed to zoom to project at end of import
+
+static constexpr unsigned int kCmdId_ZoomProject = 40295;
+static bool isAafImport = false;    // prevent zooming ALL projects
+
+static void OnProjectLoadTimer() {
+    plugin_register("-timer", reinterpret_cast<void *>(OnProjectLoadTimer));
+    // project is fully loaded here
+    if (isAafImport) {
+        Main_OnCommand(kCmdId_ZoomProject, 0);
+        isAafImport = false;
+    }
+}
+
+static void OnBeginLoadProjectState(const bool isUndo, project_config_extension_t*) {
+    if (!isUndo && isAafImport)
+        plugin_register("timer", reinterpret_cast<void *>(OnProjectLoadTimer));
+}
+
+static project_config_extension_t s_projcfg = {
+    nullptr,               // ProcessExtensionLine
+    nullptr,               // SaveExtensionConfig
+    OnBeginLoadProjectState,
+    nullptr
+};
+
+// project import stuff here
+
 static bool aaf_WantProjectFile(const char *fn) {
     const char *ext = strrchr(fn, '.');
     return ext && strcasecmp(ext, ".aaf") == 0;
 }
+
 
 static const char *aaf_EnumFileExtensions(const int i, char **descptr) {
     if (i == 0) {
@@ -47,6 +76,8 @@ static const char *aaf_EnumFileExtensions(const int i, char **descptr) {
 
 static int aaf_ImportProject(const char *fn, ProjectStateContext *ctx) {
     if (!fn || !ctx) return -1;
+    isAafImport = true;
+
     auto logBuffer = std::make_unique<LogBuffer>();
     const int ok = AafImporter(ctx, fn, *logBuffer).run();
 
@@ -62,11 +93,6 @@ static int aaf_ImportProject(const char *fn, ProjectStateContext *ctx) {
     return ok;
 }
 
-// project_import_register_t is defined in reaper_plugin.h.
-// Three function pointers in this exact order:
-//   bool        (*WantProjectFile)(const char* fn);
-//   const char* (*EnumFileExtensions)(int i, char** descptr);
-//   int         (*LoadProject)(const char* fn, ProjectStateContext* ctx);
 static project_import_register_t g_import_reg = {
     aaf_WantProjectFile,
     aaf_EnumFileExtensions,
@@ -86,6 +112,8 @@ int REAPER_PLUGIN_ENTRYPOINT(REAPER_PLUGIN_HINSTANCE hInstance,
     if (rec->caller_version != REAPER_PLUGIN_VERSION) return 0;
     if (REAPERAPI_LoadAPI(rec->GetFunc) != 0) return 0;
 
+    plugin_register("projectconfig", &s_projcfg);
+
     plugin_register("projectimport", &g_import_reg);
 
     // Register the AAF Import preferences page
@@ -93,6 +121,8 @@ int REAPER_PLUGIN_ENTRYPOINT(REAPER_PLUGIN_HINSTANCE hInstance,
 
     plugin_register("atexit", reinterpret_cast<void *>(+[] {
         PrefsPage::unregisterPage();
+        plugin_register("-projectconfig", &s_projcfg);
+        plugin_register("-projectimport", &g_import_reg);
     }));
 
     return 1;
