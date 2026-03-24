013e82eb1df37fbd4960ea1d2f30b716690d6e34
Federico Manuppella <mnp.federico@gmail.com>
2026-03-24 12:58:08 +0100

    test: add GPL license header and standardize file comment in test_progressdialog_format.cpp
    File was missing the project-wide GPL-3.0 license header and used a
    non-standard top comment. Align with the conventions every other test
    file follows.

--- tests/test_progressdialog_format.cpp
+++ tests/test_progressdialog_format.cpp
@@ -1,5 +1,21 @@
-// tests/test_progressdialog_format.cpp
-// Unit tests: formatEntry() formatting.
+/*
+ * Copyright (C) 2026 Federico Manuppella
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+// Standalone tests for ProgressDialog's formatEntry() formatting helper.
 
 #include "LogBuffer.h"
 
